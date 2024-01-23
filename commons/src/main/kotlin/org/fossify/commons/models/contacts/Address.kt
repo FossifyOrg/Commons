4f8cfa56ffb47c20bac4bec14b311cf6ae6e276c
Stephan Ritscher <stephan.ritscher@gmail.com>
2024-01-23 21:30:52 +0100

    Add support for structured address representations

--- commons/src/main/kotlin/org/fossify/commons/models/contacts/Address.kt
+++ commons/src/main/kotlin/org/fossify/commons/models/contacts/Address.kt
@@ -1,6 +1,7 @@
 package org.fossify.commons.models.contacts
-
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Address(var value: String, var type: Int, var label: String)
+data class Address(var value: String, var type: Int, var label: String, var country: String, var region: String,
+                   var city: String, var postcode: String, var pobox: String, var street: String,
+                   var neighborhood: String)
