8ee2c21510b14eff656da5685b085cfb89b3e57a
Tom Levy <tomlevy93@gmail.com>
2024-01-26 15:06:29 +0000

    Make EditText.highlightText faster by adding spans directly
    The previous code called setText() after adding each span, which is
    very slow when there is a lot of text. It is enough to call setText()
    once at the end if any matches were found.
    
    But we can do even better: we can add the highlighting spans directly
    to the existing Editable of the EditText, and skip setText() entirely,
    thus avoiding any copying of the text.
    
    The documentation of EditText.getText() says "The content of the
    return value should not be modified. If you want a modifiable one, you
    should make your own copy first." (inherited from TextView), but
    adding spans to the return value appears to work fine.

--- commons/src/main/kotlin/org/fossify/commons/extensions/EditText.kt
+++ commons/src/main/kotlin/org/fossify/commons/extensions/EditText.kt
@@ -24,7 +24,7 @@ fun EditText.onTextChangeListener(onTextChangedAction: (newText: String) -> Unit
 fun EditText.highlightText(highlightText: String, color: Int) {
     val content = text.toString()
     var indexOf = content.indexOf(highlightText, 0, true)
-    val wordToSpan = SpannableString(text)
+    val wordToSpan = text
     var offset = 0
 
     while (offset < content.length && indexOf != -1) {
@@ -36,7 +36,6 @@ fun EditText.highlightText(highlightText: String, color: Int) {
             val spanBgColor = BackgroundColorSpan(ColorUtils.setAlphaComponent(color, 128))
             val spanFlag = Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
             wordToSpan.setSpan(spanBgColor, indexOf, indexOf + highlightText.length, spanFlag)
-            setText(wordToSpan, TextView.BufferType.SPANNABLE)
         }
 
         offset = indexOf + 1
