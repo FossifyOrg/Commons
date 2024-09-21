30c646d2836c5275c84a0337197bf7c4c6f17d0d
Naveen Singh <snaveen935@gmail.com>
2024-09-21 12:24:07 +0530

    Fix ripple color and remove useless ripple alpha

--- commons/src/main/kotlin/org/fossify/commons/compose/theme/DynamicRippleConfiguration.kt
+++ commons/src/main/kotlin/org/fossify/commons/compose/theme/DynamicRippleConfiguration.kt
@@ -1,27 +1,10 @@
 package org.fossify.commons.compose.theme
 
-import androidx.compose.material.ripple.RippleAlpha
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.RippleConfiguration
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Immutable
-
-@Immutable
-internal object StateTokens {
-    const val DraggedStateLayerOpacity = 0.16f
-    const val FocusStateLayerOpacity = 0.12f
-    const val HoverStateLayerOpacity = 0.08f
-    const val PressedStateLayerOpacity = 0.12f
-}
 
 @Composable
 fun dynamicRippleConfiguration() =
     RippleConfiguration(
-        color = if (isSurfaceLitWell()) ripple_light else LocalContentColor.current,
-        rippleAlpha = RippleAlpha(
-            pressedAlpha = StateTokens.PressedStateLayerOpacity,
-            focusedAlpha = StateTokens.FocusStateLayerOpacity,
-            draggedAlpha = StateTokens.DraggedStateLayerOpacity,
-            hoveredAlpha = StateTokens.HoverStateLayerOpacity
-        )
+        color = if (isSurfaceLitWell()) ripple_light else ripple_dark,
     )
