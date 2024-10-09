9093101212383b786b2196f5addab0c56698ab47
Naveen Singh <snaveen935@gmail.com>
2024-10-09 18:48:24 +0530

    Move `DonationData` to models package

--- commons/src/main/kotlin/org/fossify/commons/compose/screens/DonationScreen.kt
+++ commons/src/main/kotlin/org/fossify/commons/compose/screens/DonationScreen.kt
@@ -19,6 +19,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.fossify.commons.R
+import org.fossify.commons.activities.FossifyCryptoAddresses
+import org.fossify.commons.activities.FossifyDonationPlatforms
 import org.fossify.commons.compose.lists.SimpleColumnScaffold
 import org.fossify.commons.compose.settings.SettingsGroup
 import org.fossify.commons.compose.settings.SettingsHorizontalDivider
