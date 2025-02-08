e757f76d92eb5f14667dd7a79b122ce098d65c19
toxynoid <1997361+toxynoid@users.noreply.github.com>
2025-02-09 00:58:35 +0100

    Add task to build.gradle.kts for publishing to local m2 cache with git commit hash.
    A task to publish build artifacts to local maven cache with git commit hash as version. Those will be picked up by gradle during next dependency sync of the root project.
    
    You must add mavenLocal() as last line to repositories of  dependencyResolutionManagement in settings.gradle.kts of your root project.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.io.ByteArrayOutputStream
+
 plugins {
     alias(libs.plugins.android).apply(false)
     alias(libs.plugins.kotlinAndroid).apply(false)
@@ -8,6 +10,22 @@ plugins {
     alias(libs.plugins.detekt).apply(false)
 }
 
+tasks.register<Exec>("setVersionToGitCommitHash") {
+    val outputStream = ByteArrayOutputStream()
+    standardOutput = outputStream
+    workingDir = project.rootDir
+    commandLine = listOf("git", "rev-parse", "--short=10", "HEAD")
+    doLast {
+        val commitHash = outputStream.toString().trim()
+        project(":commons").version = commitHash
+    }
+}
+tasks.register("publishToMavenLocalWithCommitHash") {
+    group = "Publishing"
+    description = "Publish project to local maven cache with git commit hash as version"
+    dependsOn("setVersionToGitCommitHash")
+    dependsOn("commons:publishToMavenLocal")
+}
 tasks.register<Delete>("clean") {
     delete {
         layout.buildDirectory.asFile
